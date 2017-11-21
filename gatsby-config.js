25745fc5f12e423611561b3123ce7d3e98b4d4a4
Gaël GERARD <git@gaelgerard.com>
2017-11-21 10:47:12 +0100

    test

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,68 +1,46 @@
-module.exports = {
-  siteMetadata: {
-    title: `David James`,
-    subtitle: `Front End Developer`
-  },
+// In your gatsby-config.js
   plugins: [
+    /*
+     * Gatsby's data processing layer begins with “source”
+     * plugins. Here the site sources its data from Wordpress.
+     */
     {
-      // Pull wordpress content from API
       resolve: 'gatsby-source-wordpress',
       options: {
-        baseUrl: 'dfjames.press',
+         /*
+        * The base URL of the Wordpress site without the trailingslash and the protocol. This is required.
+        * Example : 'gatsbyjsexamplewordpress.wordpress.com' or 'www.example-site.com'
+        */
+        baseUrl: 'gatsbyjsexamplewordpress.wordpress.com',
+        // The protocol. This can be http or https.
         protocol: 'http',
-        hostingWPCOM: false,
+        // Indicates whether the site is hosted on wordpress.com.
+        // If false, then the asumption is made that the site is self hosted.
+        // If true, then the plugin will source its content on wordpress.com using the JSON REST API V2.
+        // If your site is hosted on wordpress.org, then set this to false.
+        hostingWPCOM: true,
+        // If useACF is true, then the source plugin will try to import the Wordpress ACF Plugin contents.
+        // This feature is untested for sites hosted on Wordpress.com.
+        // Defaults to true.
         useACF: true,
+        auth: {
+          // If auth.user and auth.pass are filled, then the source plugin will be allowed
+          // to access endpoints that are protected with .htaccess.
+          htaccess_user: 'your-htaccess-username',
+          htaccess_pass: 'your-htaccess-password',
+          htaccess_sendImmediately: false,
+
+          // If hostingWPCOM is true then you will need to communicate with wordpress.com API
+          // in order to do that you need to create an app (of type Web) at https://developer.wordpress.com/apps/
+          // then add your clientId, clientSecret, username, and password here
+          wpcom_app_clientSecret: 'NMPnXYFtj2gKas7V1kZyMxr7oLry9V5ZxIyBQGu2txjVHg0GhFz6RYcKopkHICYg',
+          wpcom_app_clientId: '54793',
+          wpcom_user: 'gatsbyjswpexample@gmail.com',
+          wpcom_pass: 'very-secured-password',
+        },
         // Set verboseOutput to true to display a verbose output on `npm run develop` or `npm run build`
         // It can help you debug specific API Endpoints problems
         verboseOutput: false,
       },
     },
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: "David James - Front End Developer",
-        short_name: "David James",
-        start_url: "/",
-        background_color: "#111111",
-        theme_color: "#111111",
-        display: "minimal-ui",
-        icons: [
-          {
-            src: `/favicons/android-icon-192x192.png`,
-            sizes: `192x192`,
-            type: `image/png`,
-          },
-          {
-            src: `/favicons/android-icon-512x512.png`,
-            sizes: `512x512`,
-            type: `image/png`,
-          },
-        ],
-      },
-    },
-    `gatsby-plugin-offline`,
-    {
-      resolve: `gatsby-plugin-typography`,
-      options: {
-        pathToConfigModule: `src/utils/typography.js`,
-      }
-    },
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        //trackingId: 'UA-91072742-1',
-      },
-    },
-    {
-      // Does both SASS and Autoprefixing
-      resolve: `gatsby-plugin-postcss-sass`,
-      options: {
-        postCssPlugins: [
-          require('autoprefixer')
-        ],
-        precision: 8 // SASS default: 5
-      }
-    }
   ]
-}
